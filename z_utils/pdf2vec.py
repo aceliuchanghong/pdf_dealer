dc8d466568678ee0f86b801769fb6f462be66859
lawrence <aceliuchanghong@gmail.com>
2024-09-11 20:27:27 +0800

    feat: chat-with-pdf1

---
+++ z_utils/pdf2vec.py
@@ -0,0 +1,11 @@
+# https://github.com/ArmaanSeth/ChatPDF/blob/main/app.py
+def get_pdf_text(docs):
+    pass
+
+def get_chunks(raw_text):
+    pass
+
+def get_vectorstore(chunks):
+    pass
+def get_conversationchain(vectorstore):
+    pass
